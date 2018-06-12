6d85ae585349c2e92713d63d52072bff57e17541
Mohamed-fawzi <mohamed5283299@gmail.com>
2018-06-12 20:56:14 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -35,19 +36,46 @@ app.get('/todos/:id', (req, res) => {
     if(!todo){
       res.status(404).send();
     }
+
     res.status(200).send({todo});
   }).catch(e => res.status(400).send());
 });
 
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
+
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
     console.log('ID is not valid!');
   }
   Todo.findByIdAndRemove(id).then(todo => {
     if(!todo){
-      res.status(404).send();
+      return res.status(404).send();
+    }
+
+    res.status(200).send({todo});
+  }).catch(e => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+    console.log('ID is not valid!');
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findOneAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+    if(!todo){
+      return res.status(404).send();
     }
     res.status(200).send({todo});
   }).catch(e => res.status(400).send());
