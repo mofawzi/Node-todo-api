0d4697be6d8197767075dd221cd3642f4dc453ea
Mohamed-fawzi <mohamed5283299@gmail.com>
2018-06-12 21:16:49 +0200

    Setup separate test and dveloment envs

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
+require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const port = process.env.PORT;
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
 var app = express();
 
-const port = process.env.PORT || 3000;
+
 
 app.use(bodyParser.json());
 
