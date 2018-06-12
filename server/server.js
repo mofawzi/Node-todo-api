bca68cd68fa704ee97cd5ba1821954dd18814eb3
Mohamed-fawzi <mohamed5283299@gmail.com>
2018-06-12 14:42:03 +0200

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -16,6 +16,11 @@ app.post('/todos', (req, res) => {
   todo.save().then(doc => res.send(doc), e => res.status(400).send(e));
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, e => res.status(400).send(e))
+});
 
 app.listen(3000, () => console.log('Sterted on port: 3000'));
 
