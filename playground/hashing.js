09dbe80b57299bba31a01a4cf3acbe16ca69b2e9
Mohamed-fawzi <mohamed5283299@gmail.com>
2018-06-19 06:48:23 +0200

    Hash user password before saing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,20 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var pass = '123abc!';
 
-var token = jwt.sign(data, '123abc');
 
-console.log(token);
 
- var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+// var data = {
+//   id: 10
+// };
+// var token = jwt.sign(data, '123abc');
+//
+// console.log(token);
+//
+//  var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 // var message = 'I am user number 3';
 //
 // var hash = SHA256(message).toString();
